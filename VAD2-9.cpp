37daeb4f42a6ecee165d4aa6e6aecc12f66d0bdf
AboGM <abogm44ud@gmail.com>
2025-07-12 15:44:05 +0200

    Create VAD2-9.cpp

---
+++ VAD2-9.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int Number = 19 ;     //Update the blanks with the value assigned to Number
+  cout << Number ;      //Output the variable to the console
+  return 0;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a = 23 ;      //Assign the value 23 to a
+  int b = 20 ;      //Assign the value 20 to b
+  cout << a + b ;   //cout the desired output
+  return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int length = 45 ;
+  int width = 76 ;
+  int area = length * width ;
+  cout << "The Area of the given rectangle is " ;
+  cout << area ; 
+  return 0;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double pi = 3.14 ;
+  double radius = 8.9 ;      //radius has to be declared as a 'double'
+  double area = pi * radius * radius;
+  cout << "The Area of the given Circle is ";
+  cout << area;
+  
+  return 0;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string a = "Code" ;
+  string b = "Chef" ;
+  cout << a;
+  cout << b;
+  
+  return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  bool x = true ;
+  bool y = false ;
+  cout << x << endl;    //x and y need to be cout on separate lines
+  cout << y ;  
+  
+  return 0;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int s = 14;
+  int area = s * s;
+  int cost = area * 7;
+
+  cout << area << endl;
+  cout << cost << "$";
+
+  return 0;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a = -50 ;
+  int b = 40 ;
+  int sum = a + b;
+  int product = a*b ;
+  int quotient = a/b;
+  cout << sum << endl;
+  cout << product << endl;
+  cout << quotient;
+  
+  return 0;
+}
+
